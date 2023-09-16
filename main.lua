100843f8a3852468a6e4c6ed7b990b0d0cd0e0e7
Bartek Ciszkowski <hello@justbartek.ca>
2023-09-16 13:48:40 -0300

    bullet, spawning

--- main.lua
+++ main.lua
@@ -8,10 +8,20 @@ function love.load(arg)
     "assets/player.png")
   
     enemies = {}
-    enemySpawnChance = 0.02 -- 0.01
+    enemySpawnChance = 0.01
+    
+    lasers = {}
+    weaponInterval = 0
+    weaponRoF = 8
 end
 
 function love.update(dt)
+    
+  if love.keyboard.isDown("p") then
+    for i = #enemies, 1, -1 do
+      table.remove(enemies, i)
+    end
+  end
   
   -- make enemies pursue the player
   for i, v in ipairs(enemies) do
@@ -27,6 +37,7 @@ function love.update(dt)
   elseif love.keyboard.isDown("s") or love.keyboard.isDown("down") then
     inputVelocity.x = 1    
   end
+
   
   inputVelocity = inputVelocity:rotated(player.orientation) * 100
   player.acceleration = steering.velocityMath(player, {velocity = inputVelocity}, 100, 0.1)
@@ -34,28 +45,62 @@ function love.update(dt)
   -- enemy spawner
   if math.random() < enemySpawnChance then
     if math.random() < 0.1 then
-      local smallX, smallY = math.random(0, 800), math.random(0, 600)
-      for i=1,10 do
-        enemies[#enemies+1]  = Entity(smallX + math.random(-50, 50), smallY + math.random(-50, 50), "assets/wiggles.png")
+      local smallX, smallY = math.random(400 , 800), math.random(0, 600)
+      for i=1,2 do
+        enemies[#enemies+1]  = Entity(smallX + math.random(-50, 50), smallY + math.random(-50, 50), "assets/bufbloody.png")
       end
     else
-      -- spawn other enemies
-      -- enemies[#enemies+1] = Entity(math.random(0, 800), math.random(0, 600)
+      -- TODO: spawn other enemies
+      enemies[#enemies+1] = Entity(math.random(0, 800), math.random(0, 600), "assets/bufbloody.png")
     end        
   end
   
+  -- player weapon controls
+  if weaponInterval > 0 then
+		weaponInterval = weaponInterval - dt
+	end
+  if love.keyboard.isDown("space") then
+    if weaponInterval <= 0 then
+      local laser = Entity(player.position.x, player.position.y, "assets/builit.png")
+      laser.orientation = player.orientation
+      laser.speed = 400
+      laser.velocity = vector(math.sin(laser.orientation), -math.cos(laser.orientation)) * laser.speed
+      lasers[#lasers+1] = laser
+      weaponInterval = 1/weaponRoF
+    end
+  end
   
   player:update(dt)
   
   for i = #enemies, 1, -1 do
     enemies[i]:update(dt)
   end
+  
+  for i = #lasers, 1, -1 do
+    lasers[i]:update(dt)
+  end
+  
+  -- simple collision check between lasers and enemies
+	for i = #lasers, 1, -1 do
+		for j = #enemies, 1, -1 do
+			if lasers[i].position:dist(enemies[j].position) < enemies[j].width/2 then
+				table.remove(lasers, i)
+				table.remove(enemies, j)
+				enemySpawnChance = enemySpawnChance + 0.001
+				break
+			end
+		end
+	end
 end
 
 function love.draw()
   player:draw()
   
   for _, v in ipairs(enemies) do
+    v:draw()
+  end
+  
+  for _, v in ipairs(lasers) do
     v:draw()
   end
 end
